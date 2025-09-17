8daabdbcabe1e3baa5e7b4af045d47d9e0a81ec6
desktopgame <>
2025-09-17 23:03:07 +0900

    Fix: 微修正

--- internal/tui/center.go
+++ internal/tui/center.go
@@ -35,12 +35,11 @@ func (c *Center) Move(x int, y int) {
 }
 
 func (c *Center) Layout(width int, height int) {
-	mw, mh := c.Control.MinimumSize(c.Width, c.Height)
 	c.Control.Move(
-		c.x+(width-mw)/2,
-		c.y+(height-mh)/2,
+		c.x+(width-c.Width)/2,
+		c.y+(height-c.Height)/2,
 	)
-	c.Control.Layout(mw, mh)
+	c.Control.Layout(c.Width, c.Height)
 }
 
 func (c *Center) IsFlexibleWidth() bool {
