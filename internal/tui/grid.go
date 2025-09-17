467c25989a47596fd8a6b4cf0a87c26b27b37926
desktopgame <>
2025-09-17 11:51:33 +0900

    Add: Gridのテストを追加

--- internal/tui/grid.go
+++ internal/tui/grid.go
@@ -1,6 +1,9 @@
 package tui
 
-import "github.com/gdamore/tcell/v2"
+import (
+	"github.com/desktopgame/ckro/internal/tui/presenter"
+	"github.com/gdamore/tcell/v2"
+)
 
 type GridCell struct {
 	TextBox       *TextBox
@@ -25,6 +28,8 @@ type Grid struct {
 	table       [][]GridCell
 	x           int
 	y           int
+	width       int
+	height      int
 }
 
 func (g *Grid) Init(rowCount int, columnCount int) {
@@ -34,7 +39,14 @@ func (g *Grid) Init(rowCount int, columnCount int) {
 	for i := 0; i < rowCount; i++ {
 		line := []GridCell{}
 		for j := 0; j < columnCount; j++ {
-			line = append(line, GridCell{})
+			tb := TextBox{}
+			tb.Init()
+
+			tp := presenter.FrameTextPresenter{}
+			line = append(line, GridCell{
+				TextBox:       &tb,
+				TextPresenter: &tp,
+			})
 		}
 		g.table = append(g.table, line)
 	}
@@ -68,7 +80,23 @@ func (g *Grid) Update() {
 	}
 }
 
-func (g *Grid) Draw(s tcell.Screen) {}
+func (g *Grid) Draw(s tcell.Screen) {
+	for _, row := range g.table {
+		for _, c := range row {
+			c.TextBox.Draw(s)
+		}
+	}
+
+	for x := g.x; x < g.x+g.width; x++ {
+		s.SetContent(x, 0, '-', nil, tcell.StyleDefault)
+		s.SetContent(x, g.y+g.height-1, '@', nil, tcell.StyleDefault)
+	}
+
+	for y := g.y; y < g.y+g.height; y++ {
+		s.SetContent(0, y, '|', nil, tcell.StyleDefault)
+		s.SetContent(g.width-1, y, '|', nil, tcell.StyleDefault)
+	}
+}
 
 func (g *Grid) MinimumSize(width int, height int) (Width int, Height int) {
 	return width, height
@@ -79,7 +107,43 @@ func (g *Grid) Move(x int, y int) {
 	g.y = y
 }
 
-func (g *Grid) Layout(w int, h int) {}
+func (g *Grid) Layout(w int, h int) {
+	xBorders := g.columnCount + 1
+	yBorders := g.rowCount + 1
+
+	offsetY := g.y + 1
+	yMod := (h - yBorders) % g.rowCount
+	for i := 0; i < g.rowCount; i++ {
+		consumeY := min(yMod, yMod/g.rowCount)
+
+		offsetX := g.x + 1
+		xMod := (w - xBorders) % g.columnCount
+		for j := 0; j < g.columnCount; j++ {
+			gc := g.table[i][j]
+			gc.TextBox.X = offsetX
+			gc.TextBox.Y = offsetY
+			gc.TextBox.Width = (w - xBorders) / g.columnCount
+			gc.TextBox.Height = (h - yBorders) / g.rowCount
+
+			if xMod > 0 {
+				consumeX := min(xMod, xMod/g.columnCount)
+				gc.TextBox.Width += consumeX
+				xMod -= consumeX
+			}
+
+			if yMod > 0 {
+				gc.TextBox.Height += consumeY
+			}
+
+			offsetX += gc.TextBox.Width + 1
+		}
+		yMod -= consumeY
+		offsetY += ((h - yBorders) / g.rowCount) + consumeY + 1
+	}
+
+	g.width = w
+	g.height = h
+}
 
 func (g *Grid) IsFlexibleWidth() bool {
 	return false
---
+++ internal/tui/grid_test.go
@@ -0,0 +1,46 @@
+package tui_test
+
+import (
+	"testing"
+
+	"github.com/desktopgame/ckro/internal/tui"
+	"github.com/desktopgame/ckro/internal/tui/presenter"
+)
+
+func assertPos(t *testing.T, gc *tui.GridCell, x int, y int) {
+	if gc.TextBox.X != x {
+		t.Fatalf("got %q, want %q", gc.TextBox.X, x)
+	}
+	if gc.TextBox.Y != y {
+		t.Fatalf("got %q, want %q", gc.TextBox.Y, y)
+	}
+}
+
+func TestGrid(t *testing.T) {
+	g := tui.Grid{}
+	g.Init(2, 2)
+	c00 := g.Set(0, 0, &presenter.EditTextPresenter{})
+	c01 := g.Set(0, 1, &presenter.EditTextPresenter{})
+	c10 := g.Set(1, 0, &presenter.EditTextPresenter{})
+	c11 := g.Set(1, 1, &presenter.EditTextPresenter{})
+	g.Layout(11, 11)
+
+	/*
+		0----------------------------------------------------
+		1
+		2
+		3
+		4
+		5----------------------------------------------------
+		6
+		7
+		8
+		9
+		10---------------------------------------------------
+	*/
+
+	assertPos(t, c00, 1, 1)
+	assertPos(t, c01, 6, 1)
+	assertPos(t, c10, 1, 6)
+	assertPos(t, c11, 6, 6)
+}
