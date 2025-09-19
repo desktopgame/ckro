de0b2efcfc2c0eb03d57316cb7aa68841892a1e0
desktopgame <>
2025-09-20 02:00:07 +0900

    Add: clipにコメントを追加

--- internal/tui/clip.go
+++ internal/tui/clip.go
@@ -2,6 +2,7 @@ package tui
 
 import "github.com/gdamore/tcell/v2"
 
+// Clip is clipping specified region in Graphics.
 type Clip struct {
 	Graphics *Graphics
 	X        int
@@ -10,10 +11,14 @@ type Clip struct {
 	Height   int
 }
 
+// SetContent is set a character to specified cell, if not already settled.
+// TODO: refactor
 func (c Clip) SetContent(x int, y int, primary rune, combining []rune, style tcell.Style) {
 	c.Graphics.Draw(c.X+x, c.Y+y, primary, combining, style)
 }
 
+// SetCursor is set a character to specified cell.
+// TODO: refactor
 func (c Clip) SetCursor(x int, y int, primary rune, combining []rune, style tcell.Style) {
 	c.Graphics.ForceDraw(c.X+x, c.Y+y, primary, combining, style)
 }
