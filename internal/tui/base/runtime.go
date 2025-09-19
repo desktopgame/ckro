64fd1f235c024b1ac69c663e12a90c9e378f0740
desktopgame <>
2025-09-19 23:42:50 +0900

    Add: runtimeにコメントを追加

--- internal/tui/base/runtime.go
+++ internal/tui/base/runtime.go
@@ -1,9 +1,18 @@
 package base
 
+// Runtime is provide interface to for driving application.
+// in this, contain supports stack of controls.
+// controls able to erase self from stack through this Runtime object.
 type Runtime interface {
 	Stackable
 
+	// BeginBackground is increment number of background tasks.
+	// Runtime is ignore events while zero than bigger of number of background tasks.
 	BeginBackground()
+
+	// EndBackground is decrement number of background tasks.
 	EndBackground()
+
+	// Repaint is do request rerender terminal.
 	Repaint()
 }
