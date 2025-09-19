38dec7fb7145e620f8703f511a3f7db75bf917ac
desktopgame <>
2025-09-20 02:10:41 +0900

    Add: text_presenterにコメントを追加

--- internal/tui/text_presenter.go
+++ internal/tui/text_presenter.go
@@ -5,9 +5,20 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
+// TextPresenter is show fancy content through TextBox object.
+// in example, show frame by text, show tree by text, etc.
+// in addition, TextPresenter is define event process.
+// input a string when event, or change selected item, or something.
 type TextPresenter interface {
+	// Present is set shown content to View.
 	Present(view presenter.View)
+
+	// Handle is process a event.
 	Handle(view presenter.View, ev tcell.Event)
+
+	// ShowCursor returns true if should be show cursor.
 	ShowCursor() bool
+
+	// IsFocusable returns true if should be capture the focus.
 	IsFocusable() bool
 }
