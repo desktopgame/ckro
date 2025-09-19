27a7a010e06dc43a52132a191697bf2f4b6fbee8
desktopgame <>
2025-09-19 22:37:29 +0900

    Add: コントロールにコメントを追加

--- internal/tui/base/control.go
+++ internal/tui/base/control.go
@@ -1,14 +1,29 @@
 package base
 
+// Control is graphical unit for compose a screen.
+// Control can be contain another Control, but no distinction to interface by the see outer.
 type Control interface {
+	// Traverse is register sub controls to FocusManager by the order by focus.
 	Traverse(fm *FocusManager)
+
+	// Update is prepare for draw to terminal.
 	Update()
+
+	// Draw is rendering Control as text, to terminal.
 	Draw(g *Graphics)
 
+	// MinimumSize is provide size of needs to print Control.
 	MinimumSize(width int, height int) (Width int, Height int)
+
+	// Move is move a Control to specified position.
 	Move(x int, y int)
+
+	// Layout is relayout sub controls within specified size.
 	Layout(w int, h int)
 
+	// IsFlexibleWidth is returns true if Control is flexible on horizontal.
 	IsFlexibleWidth() bool
+
+	// IsFlexibleHeight is returns true if Control is flexible on vertical.
 	IsFlexibleHeight() bool
 }
