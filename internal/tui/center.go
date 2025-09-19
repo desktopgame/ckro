c43b9f6155636044e97a63d1e93416ace7a740e9
desktopgame <>
2025-09-20 08:29:14 +0900

    Add: centerにコメントを追加

--- internal/tui/center.go
+++ internal/tui/center.go
@@ -1,5 +1,6 @@
 package tui
 
+// Center is Control for centering.
 type Center struct {
 	Control Control
 	Width   int
@@ -8,28 +9,34 @@ type Center struct {
 	y       int
 }
 
+// Traverse is delegate to sub control.
 func (c *Center) Traverse(fm *FocusManager) {
 	c.Control.Traverse(fm)
 }
 
+// Update is delegate to sub control.
 func (c *Center) Update() {
 	c.Control.Update()
 }
 
+// Draw is delegate to sub control.
 func (c *Center) Draw(g *Graphics) {
 	c.Control.Draw(g)
 }
 
+// MinimumSize is provide Control minimum size with margin.
 func (c *Center) MinimumSize(width int, height int) (Width int, Height int) {
 	mw, mh := c.Control.MinimumSize(width, height)
 	return max(mw, c.Width), max(mh, c.Height)
 }
 
+// Move is delegate to sub control.
 func (c *Center) Move(x int, y int) {
 	c.x = x
 	c.y = y
 }
 
+// Layout is align center sub control to parent.
 func (c *Center) Layout(width int, height int) {
 	c.Control.Move(
 		c.x+(width-c.Width)/2,
@@ -38,10 +45,12 @@ func (c *Center) Layout(width int, height int) {
 	c.Control.Layout(c.Width, c.Height)
 }
 
+// IsFlexibleWidth returns false.
 func (c *Center) IsFlexibleWidth() bool {
 	return false
 }
 
+// IsFlexibleHeight returns false.
 func (c *Center) IsFlexibleHeight() bool {
 	return false
 }
