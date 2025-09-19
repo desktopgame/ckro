2775adabfdcb03cbd61c49a9cf2c792fdce24a9a
desktopgame <>
2025-09-19 20:46:53 +0900

    Add: Cardを追加

---
+++ internal/tui/card.go
@@ -0,0 +1,48 @@
+package tui
+
+type Card struct {
+	Controls []Control
+	Index    int
+}
+
+func (c *Card) Init(controls []Control) {
+	c.Controls = make([]Control, len(controls))
+	copy(c.Controls, controls)
+	c.Index = 0
+}
+
+func (c *Card) Target() Control {
+	return c.Controls[c.Index]
+}
+
+func (c *Card) Traverse(fm *FocusManager) {
+	c.Target().Traverse(fm)
+}
+
+func (c *Card) Update() {
+	c.Target().Update()
+}
+
+func (c *Card) Draw(g *Graphics) {
+	c.Target().Draw(g)
+}
+
+func (c *Card) MinimumSize(width int, height int) (Width int, Height int) {
+	return c.Target().MinimumSize(width, height)
+}
+
+func (c *Card) Move(x int, y int) {
+	c.Target().Move(x, y)
+}
+
+func (c *Card) Layout(w int, h int) {
+	c.Target().Layout(w, h)
+}
+
+func (c *Card) IsFlexibleWidth() bool {
+	return c.Target().IsFlexibleWidth()
+}
+
+func (c *Card) IsFlexibleHeight() bool {
+	return c.Target().IsFlexibleHeight()
+}
