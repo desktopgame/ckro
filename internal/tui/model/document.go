70ff8a0dad68051499fbfa16d1dbe3e604206ad1
desktopgame <>
2025-09-20 01:40:38 +0900

    Add: documentのコメントを追加

--- internal/tui/model/document.go
+++ internal/tui/model/document.go
@@ -2,12 +2,15 @@ package model
 
 import "github.com/desktopgame/ckro/internal/text"
 
+// Document is wrapper of Buffer.
+// track a current cursor position.
 type Document struct {
 	buffer       Buffer
 	cursorRow    int
 	cursorColumn int
 }
 
+// Init is initialize Buffer.
 func (doc *Document) Init() {
 	doc.buffer = Buffer{}
 	doc.buffer.Init()
@@ -15,6 +18,7 @@ func (doc *Document) Init() {
 	doc.cursorColumn = 0
 }
 
+// InsertLine is break line at current cursor position.
 func (doc *Document) InsertLine() {
 	currLine := doc.buffer.GetLineAt(doc.cursorRow)
 	codepointColumn := text.GraphemeToCodepointPos(currLine.GetContent(), doc.cursorColumn)
@@ -24,6 +28,7 @@ func (doc *Document) InsertLine() {
 	doc.cursorColumn = 0
 }
 
+// InsertString is insert string at current cursor position.
 func (doc *Document) InsertString(s string) {
 	currLine := doc.buffer.GetLineAt(doc.cursorRow)
 	codepointColumn := text.GraphemeToCodepointPos(currLine.GetContent(), doc.cursorColumn)
@@ -39,6 +44,7 @@ func (doc *Document) InsertString(s string) {
 	}
 }
 
+// RemoveChar is remove character at current cursor position.
 func (doc *Document) RemoveChar() {
 	currLine := doc.buffer.GetLineAt(doc.cursorRow)
 	if doc.cursorRow > 0 {
@@ -68,6 +74,7 @@ func (doc *Document) RemoveChar() {
 	}
 }
 
+// MoveLeft is move cursor to left.
 func (doc *Document) MoveLeft() {
 	if doc.cursorColumn > 0 {
 		doc.cursorColumn--
@@ -79,6 +86,7 @@ func (doc *Document) MoveLeft() {
 	}
 }
 
+// MoveRight is move cursor to right.
 func (doc *Document) MoveRight() {
 	currLine := doc.buffer.GetLineAt(doc.cursorRow)
 	if doc.cursorColumn < text.GraphemeLength(currLine.GetContent()) {
@@ -91,6 +99,7 @@ func (doc *Document) MoveRight() {
 	}
 }
 
+// MoveUp is move cursor to up.
 func (doc *Document) MoveUp() {
 	if doc.cursorRow > 0 {
 		doc.cursorRow--
@@ -103,6 +112,7 @@ func (doc *Document) MoveUp() {
 	}
 }
 
+// MoveDown is move cursor to down.
 func (doc *Document) MoveDown() {
 	if doc.cursorRow < doc.buffer.GetLineCount()-1 {
 		doc.cursorRow++
@@ -115,19 +125,23 @@ func (doc *Document) MoveDown() {
 	}
 }
 
+// MoveReset is cursor position reset to zero.
 func (doc *Document) MoveReset() {
 	doc.cursorRow = 0
 	doc.cursorColumn = 0
 }
 
+// GetBuffer returns Buffer.
 func (doc *Document) GetBuffer() *Buffer {
 	return &doc.buffer
 }
 
+// GetCursorRow returns row of cursor.
 func (doc *Document) GetCursorRow() int {
 	return doc.cursorRow
 }
 
+// GetCursorColumn returns column of cursor.
 func (doc *Document) GetCursorColumn() int {
 	return doc.cursorColumn
 }
