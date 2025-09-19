94511012e797de58b3be6a6f6c9f60c081a4335f
desktopgame <>
2025-09-19 23:53:46 +0900

    Add: stackableにコメントを追加

--- internal/tui/base/stackable.go
+++ internal/tui/base/stackable.go
@@ -1,12 +1,18 @@
 package base
 
+// Layer is stackable object.
 type Layer struct {
 	Control         Control
 	ClearBackground bool
 	OnPop           func(returnCode int)
 }
 
+// Stackable is stack of layer.
+// by doing layers draw by index order, to represent controls overlay.
 type Stackable interface {
+	// Push is stack a layer.
 	Push(layer Layer)
+
+	// Pop is remove layer from stack top.
 	Pop(returnCode int)
 }
