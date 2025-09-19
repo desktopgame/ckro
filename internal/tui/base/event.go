652ee94dcca54351d9ba61c61a28517c0f6d6c07
desktopgame <>
2025-09-19 22:46:05 +0900

    Add: eventにコメントを追加

--- internal/tui/base/event.go
+++ internal/tui/base/event.go
@@ -4,20 +4,26 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
+// Event is notify a terminal updates.
 type Event struct {
 	runtime Runtime
 	source  tcell.Event
 }
 
+// Init is initialize Event.
 func (ev *Event) Init(runtime Runtime, source tcell.Event) {
 	ev.runtime = runtime
 	ev.source = source
 }
 
+// GetRuntime returns Runtime.
+// each controls is able to access to stack of control through this object.
+// in example, use when needs to controls show modal another controls.
 func (ev *Event) GetRuntime() Runtime {
 	return ev.runtime
 }
 
+// GetSource returns raw event of tcell.
 func (ev *Event) GetSource() tcell.Event {
 	return ev.source
 }
