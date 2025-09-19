2b14c51ed2c3f18e920f6350b4e51eb2ee257365
desktopgame <>
2025-09-19 23:08:19 +0900

    Add: focus_managerにコメントを追加

--- internal/tui/base/focus_manager.go
+++ internal/tui/base/focus_manager.go
@@ -1,41 +1,57 @@
 package base
 
+// Focusable has distinction of focus on and off, and capture a Event.
 type Focusable interface {
 	Handle(ev Event)
 	Focus(on bool)
 }
 
+// FocusableTree is manage focus to sub controls.
+// if implemented this interface, parent controls stealing event, but movable focus to each children.
+// parent controls can be delegate event to children as needed.
 type FocusableTree interface {
 	Focusable
 
+	// SubFocusFirst is focus to first children.
 	SubFocusFirst()
+
+	// SubFocusPrev is focus to previous children.
 	SubFocusPrev() bool
+
+	// SubFocusNext is focus to next children.
 	SubFocusNext() bool
+
+	// SubFocusLast is focus to last children.
 	SubFocusLast()
 }
 
+// FocusManager is manage focus order in controls.
 type FocusManager struct {
 	tiles  []Focusable
 	active int
 	tree   FocusableTree
 }
 
+// Init is initialize FocusManager.
 func (fm *FocusManager) Init() {
 	fm.tiles = []Focusable{}
 	fm.active = 0
 }
 
+// Register is Focusable entry add  to table.
 func (fm *FocusManager) Register(t Focusable) {
 	fm.tiles = append(fm.tiles, t)
 	// t.GetTextBox().ShowCursor = false
 	t.Focus(false)
 }
 
+// Traverse is add all controls to table by the order of focus.
 func (fm *FocusManager) Traverse(ctrl Control) {
 	fm.Init()
 	ctrl.Traverse(fm)
 }
 
+// Grab is try to activate focus on current object.
 func (fm *FocusManager) Grab() {
 	if len(fm.tiles) == 0 {
 		return
@@ -51,6 +67,7 @@ func (fm *FocusManager) Grab() {
 	}
 }
 
+// FocusPrev is move focus to previous.
 func (fm *FocusManager) FocusPrev() {
 	if len(fm.tiles) == 0 {
 		return
@@ -82,6 +99,7 @@ func (fm *FocusManager) FocusPrev() {
 	}
 }
 
+// FocusNext is move focus to next.
 func (fm *FocusManager) FocusNext() {
 	if len(fm.tiles) == 0 {
 		return
@@ -113,6 +131,7 @@ func (fm *FocusManager) FocusNext() {
 	}
 }
 
+// Handle is process the event by focused control.
 func (fm *FocusManager) Handle(ev Event) {
 	if len(fm.tiles) == 0 {
 		return
