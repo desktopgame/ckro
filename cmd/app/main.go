666381449db6ea15aeba60a9d449e64d9c0add7b
desktopgame <>
2025-09-20 11:09:24 +0900

    Add: 引数を追加

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"flag"
+	"os"
+)
+
 func main() {
+	var (
+		directory = flag.String("d", ".", "working directory")
+	)
+	flag.Parse()
+	if *directory != "." {
+		os.Chdir(*directory)
+	}
+
 	app := Application{}
 	app.Init()
 	app.Run()
