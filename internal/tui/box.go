5577de049f25504986b4d588fd44584b27441117
desktopgame <>
2025-09-20 09:04:49 +0900

    Add: boxにコメントを追加

--- internal/tui/box.go
+++ internal/tui/box.go
@@ -1,5 +1,6 @@
 package tui
 
+// Box is layout sub controls by horizontal or vertical.
 type Box struct {
 	Controls    []Control
 	orientation Orientation
@@ -7,29 +8,35 @@ type Box struct {
 	y           int
 }
 
+// Init is initialize Box.
 func (b *Box) Init(orientation Orientation) {
 	b.Controls = []Control{}
 	b.orientation = orientation
 }
 
+// Traverse is register sub controls into FocusManager by the array order.
 func (b *Box) Traverse(fm *FocusManager) {
 	for _, ctrl := range b.Controls {
 		ctrl.Traverse(fm)
 	}
 }
 
+// Update is delegate to sub controls.
 func (b *Box) Update() {
 	for _, ctrl := range b.Controls {
 		ctrl.Update()
 	}
 }
 
+// Draw is delegate to sub controls.
 func (b *Box) Draw(g *Graphics) {
 	for _, ctrl := range b.Controls {
 		ctrl.Draw(g)
 	}
 }
 
+// MinimumSize is calculate minimum size in consideration flex.
+// flexible controls assigned evenly divided size of subtract static size from total size.
 func (b *Box) MinimumSize(width int, height int) (Width int, Height int) {
 	mw := width
 	mh := height
@@ -106,11 +113,13 @@ func (b *Box) MinimumSize(width int, height int) (Width int, Height int) {
 	return mw, mh
 }
 
+// Move is move the base point of Box.
 func (b *Box) Move(x int, y int) {
 	b.x = x
 	b.y = y
 }
 
+// Layout is arrangement sub controls into line by horizontal or vertical.
 func (b *Box) Layout(width int, height int) {
 	switch b.orientation {
 	case Horizontal:
@@ -188,6 +197,7 @@ func (b *Box) Layout(width int, height int) {
 	}
 }
 
+// IsFlexibleWidth returns true if have to any flexible width controls.
 func (b *Box) IsFlexibleWidth() bool {
 	for _, ctrl := range b.Controls {
 		if ctrl.IsFlexibleWidth() {
@@ -197,6 +207,7 @@ func (b *Box) IsFlexibleWidth() bool {
 	return false
 }
 
+// IsFlexibleHeight returns true if have to any flexible height controls.
 func (b *Box) IsFlexibleHeight() bool {
 	for _, ctrl := range b.Controls {
 		if ctrl.IsFlexibleHeight() {
@@ -206,6 +217,7 @@ func (b *Box) IsFlexibleHeight() bool {
 	return false
 }
 
+// GetOrientation returns orientation.
 func (b *Box) GetOrientation() Orientation {
 	return b.orientation
 }
