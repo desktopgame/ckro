c169afb8741188850b693f52a04e6c73efb1c9c0
desktopgame <>
2025-09-20 09:15:01 +0900

    Add: controlにコメントを追加

--- internal/tui/control.go
+++ internal/tui/control.go
@@ -2,8 +2,17 @@ package tui
 
 import "github.com/desktopgame/ckro/internal/tui/base"
 
+// Control is re-export from base.Control.
 type Control = base.Control
+
+// FocusManager is re-export from base.FocusManager.
 type FocusManager = base.FocusManager
+
+// Event is re-export from base.Event.
 type Event = base.Event
+
+// Layer is re-export from base.Layer.
 type Layer = base.Layer
+
+// Graphics is re-export from base.Graphics.
 type Graphics = base.Graphics
