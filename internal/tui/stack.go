b832eb2fcbeafd2abe431ef338e67b41a44c1699
desktopgame <>
2025-09-20 09:30:54 +0900

    Add: stackにコメントを追加

--- internal/tui/stack.go
+++ internal/tui/stack.go
@@ -1,18 +1,22 @@
 package tui
 
+// Stack is stack of layer.
 type Stack struct {
 	Layers []Layer
 	Top    int
 }
 
+// Init is initialize Stack.
 func (st *Stack) Init() {
 	st.Layers = []Layer{}
 }
 
+// Traverse is delegate to top layer.
 func (st *Stack) Traverse(fm *FocusManager) {
 	fm.Traverse(st.Layers[st.Top].Control)
 }
 
+// Update is update all layers or update top layer only, if top layer's ClearBackground is true.
 func (st *Stack) Update() {
 	if st.Top > 0 && st.Layers[st.Top].ClearBackground {
 		st.Layers[st.Top].Control.Update()
@@ -23,6 +27,7 @@ func (st *Stack) Update() {
 	}
 }
 
+// Draw is draw all layers or draw top layer only, if top layer's ClearBackground is true.
 func (st *Stack) Draw(g *Graphics) {
 	if st.Top > 0 && st.Layers[st.Top].ClearBackground {
 		st.Layers[st.Top].Control.Draw(g)
@@ -33,12 +38,14 @@ func (st *Stack) Draw(g *Graphics) {
 	}
 }
 
+// Layout is layout all layers.
 func (st *Stack) Layout(width int, height int) {
 	for i := 0; i <= st.Top; i++ {
 		st.Layers[i].Control.Layout(width, height)
 	}
 }
 
+// MinimumSize returns maximum of minimum size of each layers.
 func (st *Stack) MinimumSize(width int, height int) (Width int, Height int) {
 	w := -1
 	h := -1
