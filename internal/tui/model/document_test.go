2d58b13bb4d60864230c53a5f837804668f53086
desktopgame <>
2025-09-20 13:04:12 +0900

    Add: テストを追加

--- internal/tui/model/document_test.go
+++ internal/tui/model/document_test.go
@@ -19,3 +19,21 @@ func TestDocument(t *testing.T) {
 	cursorColumn = doc.GetCursorColumn()
 	assert.Equal(t, cursorColumn, 4)
 }
+
+func TestReplace(t *testing.T) {
+	doc := model.Document{}
+	doc.Init()
+
+	doc.InsertString("Hello1")
+	doc.InsertLine()
+
+	doc.InsertString("Hello2")
+	doc.InsertLine()
+
+	doc.InsertString("Hello3")
+	doc.InsertLine()
+
+	doc.MoveReset()
+	assert.True(t, doc.FindNext("Hello2"))
+	assert.True(t, doc.Replace(6, "Hello\nNewLine\nNewLine"))
+}
