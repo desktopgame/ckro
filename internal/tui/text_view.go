9cbcfe7ccd9b7834882e63757d65f549529414dd
desktopgame <>
2025-09-23 15:43:24 +0900

    Fix: 微修正

--- internal/tui/text_view.go
+++ internal/tui/text_view.go
@@ -4,8 +4,8 @@ import "github.com/desktopgame/ckro/internal/tui/model"
 
 type TextView interface {
 	Layout(e model.Element, width int) *TextLayout
-	Draw(e model.Element, textLayout TextLayout, g *Graphics)
+	Draw(textLayout *TextLayout, g *Graphics)
 
-	Width(e model.Element, textLayout TextLayout, row int) int
-	Height(e model.Element, textLayout TextLayout) int
+	Width(textLayout *TextLayout, row int) int
+	Height(textLayout *TextLayout) int
 }
