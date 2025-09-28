803c7e9b7f2a466f3b4376701b2c04b07ce332b3
desktopgame <>
2025-09-28 23:13:33 +0900

    Fix: 微修正

--- internal/tui/extensions/litemark/code_block_view.go
+++ internal/tui/extensions/litemark/code_block_view.go
@@ -122,7 +122,7 @@ func (c *CodeBlockView) MoveLeft(ctx view.Context, e model.Element, viewLocalPos
 }
 
 func (c *CodeBlockView) MoveRight(ctx view.Context, e model.Element, viewLocalPos int) int {
-	if viewLocalPos > c.MoveLength(ctx, e) {
+	if viewLocalPos >= c.MoveLength(ctx, e) {
 		return -1
 	}
 	return viewLocalPos + 1
