d27982fd3d8f4ddb8f0ef218ea9cfb0fa4792a1e
desktopgame <>
2025-09-30 23:45:30 +0900

    Add: テストを追加

--- internal/tui/text_box.go
+++ internal/tui/text_box.go
@@ -634,6 +634,26 @@ func (tb *TextBox) RemoveChar() {
 			tb.viewPosition = viewStart
 			return
 		}
+	} else if _, ok := element.(*litemark.CodeBlockElement); ok && viewLocalPos == 0 {
+		r := element.GetRange(0)
+		sg := tb.Document.Read(r)
+		bPos := view.CharacterReference{
+			StartPosition: model.Position{
+				Row:    r.StartPosition.Row,
+				Column: sg.GetSpan(0).EndColumn - 1,
+			},
+			Bytes: 1,
+		}
+		tb.bytePos = bPos
+		tb.Document.Remove(bPos.StartPosition.Row, bPos.StartPosition.Column, max(bPos.Bytes, 1))
+		//		tb.bytePos.StartPosition.Column++
+		tb.bytePos.Bytes = 0
+
+		tb.renderCache.Update(ctx, tb.Width)
+
+		_, vs, vl := tb.modelToView()
+		tb.viewPosition = vs + vl
+		return
 	}
 
 	newViewLocalPos := textView.MoveLeft(ctx, element, viewLocalPos)
