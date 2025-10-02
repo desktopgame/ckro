3aef16fb195b970f226582fa5ab70c0621199b4f
desktopgame <>
2025-10-02 14:11:26 +0900

    Remove: 不要なメソッドを削除

--- internal/tui/presenter/view.go
+++ internal/tui/presenter/view.go
@@ -20,8 +20,6 @@ type View interface {
 	TextVertical()
 	TextHorizontal()
 	TextClear()
-	// CursorTo(y int)
-	// CursorPosition() (X int, Y int, Rune rune, Combine []rune)
 	CursorUpdate()
 	InsertString(s string)
 	RemoveChar()
