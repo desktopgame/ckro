29e8ea9a2f258f945ec0f578ec374a779cc4baae
desktopgame <>
2025-10-03 20:52:07 +0900

    Update: 折りたたみの削除

--- internal/tui/text_box.go
+++ internal/tui/text_box.go
@@ -574,6 +574,26 @@ func (tb *TextBox) RemoveChar() {
 		_, vs, vl := tb.modelToView()
 		tb.viewPosition = vs + vl
 		return
+	} else if _, ok := element.(*model.FoldBlockElement); ok && viewLocalPos == 0 {
+		r := element.GetRange(0)
+		sg := tb.Document.Read(r)
+		bPos := view.CharacterReference{
+			StartPosition: model.Position{
+				Row:    r.StartPosition.Row,
+				Column: sg.GetSpan(0).EndColumn - 1,
+			},
+			Bytes: 1,
+		}
+		tb.bytePos = bPos
+		tb.Document.Remove(bPos.StartPosition.Row, bPos.StartPosition.Column, max(bPos.Bytes, 1))
+		//		tb.bytePos.StartPosition.Column++
+		tb.bytePos.Bytes = 0
+
+		tb.renderCache.Update(ctx, tb.Width)
+
+		_, vs, vl := tb.modelToView()
+		tb.viewPosition = vs + vl
+		return
 	} else if txView, ok := textView.(*litemark.TextView); ok {
 		combine, bPos := txView.RemoveCombine(ctx, tb.renderCache.GetLayout(elementIndex), viewLocalPos)
 		if combine {
