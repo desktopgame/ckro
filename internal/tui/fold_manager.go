fcd68f519c2ca5188c293105ae55f9cab06418bd
desktopgame <>
2025-10-04 20:45:53 +0900

    Update: 折りたたみ状態の制御

--- internal/tui/fold_manager.go
+++ internal/tui/fold_manager.go
@@ -3,14 +3,12 @@ package tui
 import (
 	"slices"
 
-	"github.com/desktopgame/ckro/internal/optional"
 	"github.com/desktopgame/ckro/internal/tui/model"
 )
 
 type FoldItem struct {
-	prevLine optional.Optional[string]
-	nextLine optional.Optional[string]
-	element  model.Element
+	track   *model.Track
+	element model.Element
 }
 
 type FoldManager struct {
@@ -18,22 +16,23 @@ type FoldManager struct {
 }
 
 func (fm *FoldManager) addFold(doc model.Document, e model.Element) {
-	prevLine, nextLine := fm.scan(doc, e)
+	r := e.GetRange(0)
+	tr := doc.CreateTrack(r.StartPosition.Row, r.StartPosition.Column)
 
 	fm.items = append(fm.items, FoldItem{
-		prevLine: prevLine,
-		nextLine: nextLine,
-		element:  e,
+		track:   tr,
+		element: e,
 	})
 }
 
 func (fm *FoldManager) ToggleFold(doc model.Document, e model.Element) {
-	i := fm.indexOf(doc, e)
-	if i >= 0 {
-		fm.items = slices.Delete(fm.items, i, i+1)
-	} else {
-		fm.addFold(doc, e)
+	for i, fold := range fm.items {
+		if fm.isMatchRange(fold.track, e) {
+			fm.items = slices.Delete(fm.items, i, i+1)
+			return
+		}
 	}
+	fm.addFold(doc, e)
 }
 
 func (fm *FoldManager) Refresh(doc model.Document) {
@@ -43,10 +42,9 @@ func (fm *FoldManager) Refresh(doc model.Document) {
 		if _, ok := element.(*model.FoldBlockElement); !ok {
 			continue
 		}
-		prevLine, nextLine := fm.scan(doc, element)
 
 		for _, fold := range fm.items {
-			if fold.prevLine.IsSame(prevLine) && fold.nextLine.IsSame(nextLine) {
+			if fm.isMatchRange(fold.track, element) {
 				newItems = append(newItems, fold)
 				break
 			}
@@ -56,20 +54,24 @@ func (fm *FoldManager) Refresh(doc model.Document) {
 	fm.items = newItems
 }
 
-func (fm *FoldManager) indexOf(doc model.Document, e model.Element) int {
-	prevLine, nextLine := fm.scan(doc, e)
-
-	for i, item := range fm.items {
-		if item.prevLine.IsSame(prevLine) && item.nextLine.IsSame(nextLine) {
-			return i
-		}
+func (fm *FoldManager) isMatchRange(tr *model.Track, e model.Element) bool {
+	r := e.GetRange(0)
+	if tr.Position.Row == r.StartPosition.Row && tr.Position.Column == r.StartPosition.Column {
+		return true
 	}
-
-	return -1
+	return false
 }
 
 func (fm *FoldManager) IsFolded(doc model.Document, e model.Element) bool {
-	return fm.indexOf(doc, e) >= 0
+	for _, fold := range fm.items {
+		if fold.track.Lost {
+			continue
+		}
+		if fm.isMatchRange(fold.track, e) {
+			return true
+		}
+	}
+	return false
 }
 
 func (fm *FoldManager) AutoFold(doc model.Document, elements []model.Element) {
@@ -83,42 +85,4 @@ func (fm *FoldManager) AutoFold(doc model.Document, elements []model.Element) {
 			}
 		}
 	}
-}
-
-func (fm *FoldManager) scan(doc model.Document, e model.Element) (PrevLine optional.Optional[string], NextLine optional.Optional[string]) {
-	r := e.GetRange(0)
-
-	prevLine := optional.None[string]()
-	if r.StartPosition.Row > 0 {
-		prevRange := model.Range{
-			StartPosition: model.Position{
-				Row:    r.StartPosition.Row - 1,
-				Column: 0,
-			},
-			EndPosition: model.Position{
-				Row:    r.StartPosition.Row - 1,
-				Column: doc.GetLineBytes(r.StartPosition.Row - 1),
-			},
-		}
-		sg := doc.Read(prevRange)
-		prevLine = optional.Some(sg.GetLine(0))
-	}
-
-	nextLine := optional.None[string]()
-	if r.EndPosition.Row+1 < doc.GetLineCount() {
-		prevRange := model.Range{
-			StartPosition: model.Position{
-				Row:    r.EndPosition.Row + 1,
-				Column: 0,
-			},
-			EndPosition: model.Position{
-				Row:    r.EndPosition.Row + 1,
-				Column: doc.GetLineBytes(r.EndPosition.Row + 1),
-			},
-		}
-		sg := doc.Read(prevRange)
-		nextLine = optional.Some(sg.GetLine(0))
-	}
-
-	return prevLine, nextLine
 }
