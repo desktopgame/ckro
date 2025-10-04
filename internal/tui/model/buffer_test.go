c56a360b1b7c75d96d5676f786036781be53ecb8
desktopgame <>
2025-10-04 20:32:13 +0900

    Add: テスト追加

--- internal/tui/model/buffer_test.go
+++ internal/tui/model/buffer_test.go
@@ -122,3 +122,20 @@ func TestTrack04(t *testing.T) {
 	assert.Equal(t, tr.Position.Row, 1)
 	assert.Equal(t, tr.Position.Column, 4)
 }
+
+func TestTrack05(t *testing.T) {
+	buf := model.Buffer{}
+	buf.Init()
+
+	buf.InsertString(0, 0, "123456789")
+
+	tr := buf.CreateTrack(0, 1)
+
+	buf.RemoveString(0, 2, 7)
+	assert.Equal(t, tr.Position.Row, 0)
+	assert.Equal(t, tr.Position.Column, 1)
+
+	buf.InsertString(0, 0, "ABC\nAAA\nAAA")
+	assert.Equal(t, tr.Position.Row, 2)
+	assert.Equal(t, tr.Position.Column, 4)
+}
