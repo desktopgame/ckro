791798292f35d3bc8fb359acd69de0fade7e79e7
desktopgame <>
2025-10-09 13:29:33 +0900

    Add: コメントを追加

--- internal/tui/model/segment.go
+++ internal/tui/model/segment.go
@@ -1,7 +1,13 @@
 package model
 
+// Segment is reference to multiline string.
 type Segment interface {
+	// GetSpan returns span at specified line.
 	GetSpan(lineIndex int) Span
+
+	// GetLine returns string of line at specified line.
 	GetLine(lineIndex int) string
+
+	// GetLineCount returns line count.
 	GetLineCount() int
 }
