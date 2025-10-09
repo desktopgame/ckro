6e7a8455e690f47ff87f191b66cf69c6c788b7a6
desktopgame <>
2025-10-09 13:24:23 +0900

    Add: コメントを追加

--- internal/tui/model/range.go
+++ internal/tui/model/range.go
@@ -1,10 +1,10 @@
 package model
 
+// Range is range presentation by two Position.
+// end column is exclusive.
 type Range struct {
-	// 範囲の最初の文字の最初のバイト（これを含む）
 	StartPosition Position
-	// 範囲の最後の文字の最後のバイト（これを含まない）
-	EndPosition Position
+	EndPosition   Position
 }
 
 func (r Range) IsZero() bool {
