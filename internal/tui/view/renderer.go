75bc476ee65a3e76667034b89fdc36b7af4672f0
desktopgame <>
2025-10-09 13:40:03 +0900

    Add: コメントを追加

--- internal/tui/view/renderer.go
+++ internal/tui/view/renderer.go
@@ -2,8 +2,12 @@ package view
 
 import "github.com/gdamore/tcell/v2"
 
+// Renderer is terminal graphics backend.
+// only tcell supported in currently.
 type Renderer interface {
+	// SetContent put rune at specified position.
 	SetContent(x int, y int, primary rune, combining []rune, style tcell.Style)
 
+	// Translate returns renderer of after coordinate translate.
 	Translate(offsetX int, offsetY int) Renderer
 }
