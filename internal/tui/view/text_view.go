6b8006c6f9590f97f747090fbed1858ed49f4697
desktopgame <>
2025-10-09 13:59:50 +0900

    Add: コメントを追加

--- internal/tui/view/text_view.go
+++ internal/tui/view/text_view.go
@@ -2,27 +2,45 @@ package view
 
 import "github.com/desktopgame/ckro/internal/tui/model"
 
+// TextView is render a part of text.
+// TextView is should be stateless, and every state is given from parameter.
 type TextView interface {
-	// x,y はこのビューを置くべき左上の座標
-	// w,h はこのビューに与えられたサイズ
+	// Layout is layout this view and contained sub views.
+	// x and y is origin of this view, aim left top.
+	// w and h is given layout size.
 	Layout(ctx Context, textLayout *TextLayout, x, y, w, h int)
 
-	// rendererは0,0をビューの左上として描画を開始する
+	// Draw is rendernig content.
 	Draw(ctx Context, textLayout *TextLayout, renderer Renderer)
 
-	// width,height はこのビューに与えられたサイズ
+	// MinimumSize returns TextLayout for this view and contained sub views.
+	// calculate only minimum size, at this point.
 	MinimumSize(ctx Context, e model.Element, width int, height int) *TextLayout
 
+	// MoveLength returns movable count for cursor in this view.
+	// inclusive a subview move count if this view is inclusive a subview.
 	MoveLength(ctx Context, textLayout *TextLayout) int
+
+	// MoveUp returns position after cursor up.
 	MoveUp(ctx Context, textLayout *TextLayout, viewLocalPos int) int
+
+	// MoveDown returns position after cursor down.
 	MoveDown(ctx Context, textLayout *TextLayout, viewLocalPos int) int
+
+	// MoveLeft returns position after cursor left.
 	MoveLeft(ctx Context, textLayout *TextLayout, viewLocalPos int) int
+
+	// MoveRight returns position after cursor right.
 	MoveRight(ctx Context, textLayout *TextLayout, viewLocalPos int) int
+
+	// ConvertPos returns 2D position from view local position.
 	ConvertPos(ctx Context, textLayout *TextLayout, viewLocalPos int) (ViewLocalX int, ViewLocalY int)
+
+	// ConvertModel returns character at specified view local position.
 	ConvertModel(ctx Context, textLayout *TextLayout, viewLocalPos int) CharacterReference
 
-	// バイト座標をローカルビュー座標に変換する
-	// 飾りと重なるときは内側に寄せる
+	// ConvertViewLocalPos returns view local position at specified byte position.
+	// move to inward if byte position is aim to decoration.
 	ConvertViewLocalPos(ctx Context, textLayout *TextLayout, bytePos model.Position) int
 
 	// 指定のローカルビュー位置における改行を行の前に移動するなら true を返す
