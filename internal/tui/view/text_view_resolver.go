54fe37c54c23f614849087f62bb6932edbf5f26c
desktopgame <>
2025-10-09 13:47:31 +0900

    Add: コメントを追加

--- internal/tui/view/text_view_resolver.go
+++ internal/tui/view/text_view_resolver.go
@@ -2,6 +2,8 @@ package view
 
 import "github.com/desktopgame/ckro/internal/tui/model"
 
+// TextViewResolver resolve TextView from Element.
+// in normally, determine by element type.
 type TextViewResolver interface {
 	Resolve(e model.Element) TextView
 }
