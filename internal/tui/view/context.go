7b69ae4f3ec973edcd3487c19a421378dc029881
desktopgame <>
2025-10-09 13:36:39 +0900

    Add: コメントを追加

--- internal/tui/view/context.go
+++ internal/tui/view/context.go
@@ -2,6 +2,7 @@ package view
 
 import "github.com/desktopgame/ckro/internal/tui/model"
 
+// Context is rendering parameter set of basic.
 type Context struct {
 	Resolver      TextViewResolver
 	Document      model.Document
@@ -9,15 +10,17 @@ type Context struct {
 	TextSelection TextSelection
 }
 
+// GetSegment returns Range at specified index.
 func (ctx Context) GetSegment(e model.Element, rangeIndex int) model.Segment {
 	r := e.GetRange(rangeIndex)
 	return ctx.Document.Read(r)
 }
 
+// GetText returns text of Element
 func (ctx Context) GetText(e model.Element) string {
 	sg := ctx.GetSegment(e, 0)
 	if sg.GetLineCount() != 1 {
-		panic("")
+		panic("text is many lines")
 	}
 	return sg.GetLine(0)
 }
