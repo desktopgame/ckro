a961fc064bc95f9b497a323c87f77ce60bac0243
desktopgame <>
2025-10-09 13:46:36 +0900

    Add: コメントを追加

--- internal/tui/view/text_layout.go
+++ internal/tui/view/text_layout.go
@@ -2,11 +2,11 @@ package view
 
 import "github.com/desktopgame/ckro/internal/tui/model"
 
+// TextLayout is representation a layout at a certain point in time.
 type TextLayout struct {
 	Element  model.Element
 	Children []*TextLayout
 
-	// この要素を描画するとき、親はこのオフセット分TranslateしたRendererを子に渡して描画を移譲する
 	RelativeX int
 	RelativeY int
 
@@ -14,5 +14,4 @@ type TextLayout struct {
 	Height        int
 	MinimumWidth  int
 	MinimumHeight int
-	WidthTable    []int
 }
