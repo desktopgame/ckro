f51a07c27b4b6789fda4af29c12cf0013fed6a6a
desktopgame <>
2025-10-11 00:01:28 +0900

    Fix: 折りたたみ+コードブロックの対応

--- internal/tui/view/fold_block_view.go
+++ internal/tui/view/fold_block_view.go
@@ -105,6 +105,24 @@ func (fv *FoldBlockView) Measure(ctx Context, e model.Element, width int, height
 					children = append(children, child)
 				}
 			}
+		} else if child.MinimumHeight > 1 {
+			r := childElement.GetRange(0)
+			if r.StartPosition.Row != r.EndPosition.Row {
+				r = model.Range{
+					StartPosition: r.StartPosition,
+					EndPosition: model.Position{
+						Row:    r.StartPosition.Row,
+						Column: ctx.Document.GetLineBytes(r.StartPosition.Row),
+					},
+				}
+			}
+			childElement = &model.PlainElement{
+				Range: r,
+			}
+			childView = &PlainTextView{}
+			child = childView.Measure(ctx, childElement, width-4, 9999)
+			minimumHeight += child.MinimumHeight
+			children = append(children, child)
 		} else {
 			minimumHeight++
 			children = append(children, child)
