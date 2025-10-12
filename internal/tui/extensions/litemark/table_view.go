3049d65bbb8c5fb199cbb5b1fdc00799846a6056
desktopgame <>
2025-10-12 20:09:40 +0900

    Update: テーブルを壊せるように

--- internal/tui/extensions/litemark/table_view.go
+++ internal/tui/extensions/litemark/table_view.go
@@ -377,7 +377,11 @@ func (tv *TableView) ShouldRemoveWithLine(ctx view.Context, textLayout *view.Tex
 
 func (tv *TableView) ShouldRemoveWithSpecifiedColumnAfter(ctx view.Context, textLayout *view.TextLayout, viewLocalPos int) (model.Position, bool) {
 	if viewLocalPos == 0 {
-		return model.Position{}, false
+		r := textLayout.Element.GetRange(0)
+		return model.Position{
+			Row:    r.StartPosition.Row,
+			Column: ctx.Document.GetLineBytes(r.StartPosition.Row) - 1,
+		}, true
 	}
 	table, _ := tv.moveTable(ctx, textLayout)
 	ttl := 0
