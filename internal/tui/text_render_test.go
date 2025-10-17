533e5a08642bb5b64abf73e6140ab0a75da7b91d
desktopgame <>
2025-10-17 18:04:20 +0900

    Add: テスト追加

--- internal/tui/text_render_test.go
+++ internal/tui/text_render_test.go
@@ -143,6 +143,8 @@ func testScenario(t *testing.T, scenarioFile string) string {
 			assert.Equal(t, args[0], byte('"'))
 			assert.Equal(t, args[len(args)-1], byte('"'))
 			assert.True(t, tb.FindNext(args[1:len(args)-1]))
+		case "SUBMIT":
+			assert.True(t, tb.Submit())
 		case "BYTE_POS":
 			bPosStr := strings.Split(args, " ")
 			bPosRow, err := strconv.Atoi(bPosStr[0])
